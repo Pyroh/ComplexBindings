eed6bff0ba45888d55757b6b746ac15932a4917d
Pierre TACCHI <pyroh@icloud.com>
2017-12-14 18:37:21 +0100

    Initial Commit

---
+++ ComplexBindings/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ComplexBindings
+//
+//  Created by Pierre TACCHI on 14/12/2017.
+//  Copyright © 2017 Pierre TACCHI. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ComplexBindings/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ComplexBindings
+//
+//  Created by Pierre TACCHI on 14/12/2017.
+//  Copyright © 2017 Pierre TACCHI. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
