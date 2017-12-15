31a0cb2954f56d2f09de49676ad6772d433b2c1d
Pierre TACCHI <pyroh@icloud.com>
2017-12-15 23:02:39 +0100

    Add custom ColorWell
    New NSColorWell subclass showing how to handle binding markers.

---
+++ ComplexBindings/ColorWell.swift
@@ -0,0 +1,19 @@
+//
+//  ColorWell.swift
+//  ComplexBindings
+//
+//  Created by Pierre TACCHI on 15/12/2017.
+//  Copyright © 2017 Pierre TACCHI. All rights reserved.
+//
+
+import Cocoa
+
+class ColorWell: NSColorWell {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
